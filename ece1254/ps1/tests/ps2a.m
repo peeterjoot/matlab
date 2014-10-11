8df0591dfccf767facc065cc02b993279ce7b1fb
Peeter Joot <peeter.joot@gmail.com>
2014-10-11 19:40:40 -0400

    new file:   ../ps1.2.netlist.plotted new file:   fit.m renamed:    ps2a.mat -> ps2a.m

--- ece1254/ps1/tests/ps2a.m
+++ ece1254/ps1/tests/ps2a.m
@@ -6,15 +6,16 @@ netlistFileName = 'ps1.2.netlist' ;
 
 epsilon = eps( 1.0 ) * 100 ;
 
-doProblemB = 0 ;
+doProblemB = 1 ;
 
 if ( doProblemB )
    % p2(b).  Plot times vs. N
-   gridSizes = [ 2 4 8 16 32 ] ;
+   %gridSizes = 2:4:50 ;
+   gridSizes = [ 4 8 12 16 20 24 28 32 36 40 44 ] ;
    numTimings = 1 ;
 else
    % p2(a)
-   gridSizes = [ 5 ] ;
+   gridSizes = [ 50 ] ;
    numTimings = 3 ;
 end
 numGrids = size( gridSizes, 2 ) ;
@@ -31,6 +32,7 @@ for j = 1:numGrids
    generateResistorGridNetlist( netlistFileName, gridSizes(j) ) ;
    [G, b] = NodalAnalysis( netlistFileName ) ;
 
+
    for i = 1:numTimings
       tic
       [P, L, U, s] = withPivotLU( G, epsilon ) ;
@@ -45,29 +47,59 @@ for j = 1:numGrids
       backwardTimings(i, j) = toc ;
 
       tic
-      xm = G\b
+      xm = G\b ;
       solveTimings(i, j) = toc ;
    end
+
+   % sanity check: expect something near zero:
+   %max(max(abs(G - L * U)))
 end
 
-if ( ~doProblemB )
+if ( doProblemB )
+   totalTimes = luTimings + forwardTimings + backwardTimings ;
+
+   logTotalTimes = log( totalTimes ) ;
+   logN = log( gridSizes ) ;
+
+   p = polyfit( logN, logTotalTimes, 1 ) ;
+   m = p(1) ;
+   a = p(2) ;
+
+   fitCheck = zeros(numGrids, 1) ;
+   logFitCheck = zeros(numGrids, 1) ;
+   for i = 1:numGrids
+      logFitCheck(i) = a + logN( i ) * m ;
+      fitCheck(i) = exp(a) * gridSizes(i)^m ;
+   end
+
+   m
+   exp(a)
+   figure ; plot( logN, logFitCheck, logN, logTotalTimes )
+   figure ; plot( gridSizes, fitCheck, gridSizes, totalTimes )
+
+%>> blah=[0 1]
+%     0.0000e+000     1.0000e+000
+%
+%>> blahy = [0 2]
+%     0.0000e+000     2.0000e+000
+%
+%>> polyfit( blah, blahy, 1 )
+%     2.0000e+000     0.0000e+000
+
+else
    % do the surface plot
 
    % surf() is a surface plot function, that takes three arrays, one for X coordinate, one for Y coordinates, and one
    % for the value of the function at each of these points.
    %>> [X,Y,Z] = peaks(3); figure ; surf(X,Y,Z)
    %>> X
-   %
    %X =
-   %
    %    -3.0000e+000     0.0000e+000     3.0000e+000
    %    -3.0000e+000     0.0000e+000     3.0000e+000
    %    -3.0000e+000     0.0000e+000     3.0000e+000
    %
    %>> Y
-   %
    %Y =
-   %
    %    -3.0000e+000    -3.0000e+000    -3.0000e+000
    %     0.0000e+000     0.0000e+000     0.0000e+000
    %     3.0000e+000     3.0000e+000     3.0000e+000
@@ -86,20 +118,11 @@ if ( ~doProblemB )
    for j = 1:n+1
       X(i,j) = i ;
       Y(i,j) = j ;
-      Z(i,j) = x( (N+1)(i-1) + j ) ;
+      Z(i,j) = x( (n+1) * (i-1) + j ) ;
    end
    end
-
-   X
-   Y
-   Z
+   
+   figure ;
+   surf( X, Y, Z ) ;
 end
 
-% take the minimum timing value as representitive
-%min(luTimings)
-%min(forwardTimings)
-%min(backwardTimings)
-%min(solveTimings)
-
-% sanity check: expect something near zero:
-max(max(abs(G - L * U)))
