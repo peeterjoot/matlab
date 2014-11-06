2d13a1a7bdfd76eac61bd6313ec3bbb85ab680fb
Peeter Joot <peeter.joot@gmail.com>
2014-11-05 20:28:35 -0500

    modified:   newtonsDiffusion.m
    remove the stepping code.

--- ece1254/ps2b/newtonsDiffusion.m
+++ ece1254/ps2b/newtonsDiffusion.m
@@ -1,11 +1,11 @@
-function [x, f, iter, normSqF, dxSquared, totalIters] = newtonsDiffusion( N, V, tolF, tolX, tolRel, maxIter, useStepping, alpha )
+function [x, F, iter, normSqF, dxSquared] = newtonsDiffusion( N, V, tolF, tolX, tolRel, maxIter )
 % sample call:
 % 
-%     n = 5 ; d=1/6 ; [x, f, iter, normSqF, dxSquared, totalIters] = newtonsDiffusion( n, 1, 1e-12, 1e-12, 1e-12, 1000, 0, 1 ) ; plot( d:d:n * d, f )
-%     n=100 ; d=1/101 ; [x, f, iter, normSqF, dxSquared, totalIters] = newtonsDiffusion( n, 8, 1e-12, 1e-12, 1e-12, 2000, 0, 1 ) ; plot( d:d:n * d, x )
+%     n = 5 ; d=1/6 ; [x, F, iter, normSqF, dxSquared] = newtonsDiffusion( n, 1, 1e-12, 1e-12, 1e-12, 1000 ) ; plot( d:d:n * d, x )
+%     n=100 ; d=1/101 ; [x, F, iter, normSqF, dxSquared] = newtonsDiffusion( n, 8, 1e-12, 1e-12, 1e-12, 2000 ) ; plot( d:d:n * d, x )
 %
-% solve: F(x, lambda) = G * x - lambda * b + 2 [sinh(x_i)]_i
-%        b(1) = -lambda * V, b(N) = lambda * V, b({else}) = 0.
+% solve: F(x) = G * x - b + 2 [sinh(x_i)]_i
+%        b(1) = -V, b(N) = V, b({else}) = 0.
 %        G = 2 I - P - P^T, where P is ones on the superdiagonal, and zeros elsewhere.
 % 
 % This are the nodal equations for the finite approximation of the nonlinear Poisson equation:
@@ -13,14 +13,10 @@ function [x, f, iter, normSqF, dxSquared, totalIters] = newtonsDiffusion( N, V, 
 %    d^2 psi/du^2 = 2 sinh( psi(u) ), psi(0) = -V, psi(1) = V, u in [0,1]
 %
 % N: number of sampling points (not including end points).  solution vector x has components: x_i = psi( u_i )
-% alpha: damping constant
 %
 % +-V: values at x=1,0
 %
 % maxIter: stop after this many iterations if not converged.
-%
-% useStepping: vary V \in [0,1] V, and proceed with previous estimate (otherwise use V).
-%    The values lambda are taken from the interval [0,1].
 %
 % iteration stops when all of:
 %    ||F||^2 < tolF
@@ -29,19 +25,10 @@ function [x, f, iter, normSqF, dxSquared, totalIters] = newtonsDiffusion( N, V, 
 %
 % returns:
 %    x: solution vector.
-%    f: value of (vector) function at end of the iteration.
+%    F: value of (vector) function at end of the iteration.
 %    iter: last number of iterations
 %    normSqF: ||F(x)||^2 at the end of the iteration.
 %    dxSquared: ||x||^2 at the end of the iteration.
-%    totalIters: sum of last iter counts if useStepping is set (otherwise == iter).
-
-if ( useStepping )
-   steppingIntervals = 100 ;
-
-   lambdas = 1/steppingIntervals:1/steppingIntervals:1 ;
-else
-   lambdas = [ 1 ] ;
-end
 
 nOnes = ones( N, 1 ) ;
 G = sparse( diag(2 * nOnes, 0) - diag(nOnes(1:N-1), -1) - diag(nOnes(1:N-1), 1) ) ;
@@ -54,8 +41,8 @@ b(N,1) = V ;
 
 deltaXsq = 1/(N+1)^2 ;
 % H(x) = 2 * deltaXsq * sinh( x(1:N) ) 
-% F(x) = G * x - lambda * b + H(x)
-%      = G * x - lambda * b + 2 * deltaXsq * sinh( x(1:N) )
+% F(x) = G * x - b + H(x)
+%      = G * x - b + 2 * deltaXsq * sinh( x(1:N) )
 
 fp = 2 * deltaXsq ;
 J = G + sparse( diag(fp * nOnes, 0) ) ;
@@ -63,37 +50,57 @@ J = G + sparse( diag(fp * nOnes, 0) ) ;
 % FIXME: use LU, but wait until I get base algorithm right.  
 fpinv = inv( J ) ;
 
-totalIters = 0 ;
-disp( '\\( \\lambda \\) & i & \\( \\Norm{ \\Bx } \\) & \\( \\Norm{ F(\\Bx)} \\) & \\( \\Norm{\\Delta x} \\) & \\( \\Norm{\\Delta x}/\\Norm{x} \\) \\\\ \\hline' ) ;
-for lambda = lambdas
-   iter = 0 ;
-   x = lastX ;
-   f = G * x - lambda * b + fp * sinh( x(1:N) ) ;
-   dxSquared = 0 ;
-   relDiffSqX = 0 ;
-   normSqF = f.' * f ;
+%disp( 'i & \\( \\alpha \\) & \\( \\Norm{ \\Bx } \\) & \\( \\Norm{ F(\\Bx)} \\) & \\( \\Norm{\\Delta x} \\) & \\( \\Norm{\\Delta x}/\\Norm{x} \\) \\\\ \\hline' ) ;
+disp( 'i & alpha & max |x_i| & |F| & |dx| & |dx|/|x|' ) ;
+iter = 0 ;
+x = lastX ;
+F = G * x - b + fp * sinh( x(1:N) ) ;
+dxSquared = 0 ;
+relDiffSqX = 0 ;
+normSqF = F.' * F ;
 
-   while ( iter < maxIter )
-      totalIters = totalIters + 1 ;
+while ( iter < maxIter )
+   delta = -fpinv * F ;
+   Gdelta = G * delta ;
+   Hprime = zeros(N, 1) ;
 
-      if ( ( 0 == mod(iter, 50) ) || (iter < 10) )
-         disp( sprintf( '%1.2f & %d & %f & %2.1e & %2.1e & %2.1e \\\\ \\hline', lambda, iter, norm(x), norm(f), sqrt(dxSquared), sqrt(relDiffSqX) ) ) ;
-      end
-
-      x = lastX - alpha * fpinv * f ;
-      f = G * x - lambda * b + fp * sinh( x(1:N) ) ;
-      normSqF = f.' * f ;
-      iter = iter + 1 ;
-
-      dx = x - lastX ;
-      dxSquared = dx.' * dx ;
-      relDiffSqX = dxSquared/(lastX.' * lastX) ;
-      lastX = x ;
-
-      if ( (normSqF < tolF) && (dxSquared < tolX) && (relDiffSqX < tolRel) )
-         break ;
-      end
+   % can this statement be vectorized like sinh( x(1:N) ) ?
+   for i = 1:N
+      Hprime(i) = fp * x(i) * cosh( x(i) ) ;
    end
 
-   disp( sprintf( '%1.2f & %d & %f & %2.1e & %2.1e & %2.1e \\\\ \\hline', lambda, iter, norm(x), norm(f), sqrt(dxSquared), sqrt(relDiffSqX) ) ) ;
+   Fprime = Gdelta + Hprime ;
+
+%disp( Gdelta.' * Fprime) ;
+%disp( Gdelta ) ;
+%disp( Fprime ) ;
+   alpha = -(F.' * Fprime) / ( Gdelta.' * Fprime) ;
+
+%   if ( ( 0 == mod(iter, 50) ) || (iter < 10) )
+      disp( sprintf( '%d & %f & %f & %2.1e & %2.1e & %2.1e \\\\ \\hline', iter, alpha, max(abs(x)), norm(F), sqrt(dxSquared), sqrt(relDiffSqX) ) ) ;
+%   end
+
+   x = lastX + delta ;
+%   x = lastX + alpha * delta ;
+
+%disp( sprintf( '%d: x, H = ', iter ) ) ;
+% x(0) ends up with 800, for which isinf( sinh(800) ) = 1.
+%disp( x ) ;
+   H = fp * sinh( x(1:N) ) ;
+%disp( H ) ;
+%disp( {'H', H} ) ;
+   F = G * x - b + H ;
+   normSqF = F.' * F ;
+   iter = iter + 1 ;
+
+   dx = x - lastX ;
+   dxSquared = dx.' * dx ;
+   relDiffSqX = dxSquared/(lastX.' * lastX) ;
+   lastX = x ;
+
+   if ( (normSqF < tolF) && (dxSquared < tolX) && (relDiffSqX < tolRel) )
+      break ;
+   end
 end
+
+disp( sprintf( '%d & %f & %f & %2.1e & %2.1e & %2.1e \\\\ \\hline', iter, alpha, max(abs(x)), norm(F), sqrt(dxSquared), sqrt(relDiffSqX) ) ) ;
