085c0b5ccdeeb51ee5caedc91987a1ed53060cbb
Peeter Joot <peeter.joot@gmail.com>
2014-11-06 08:40:35 -0500

    modified:   newtonsDiffusion.m
    Got it working (without damping).

--- ece1254/ps2b/newtonsDiffusion.m
+++ ece1254/ps2b/newtonsDiffusion.m
@@ -1,8 +1,7 @@
 function [x, F, iter, normSqF, dxSquared] = newtonsDiffusion( N, V, tolF, tolX, tolRel, maxIter )
 % sample call:
 % 
-%     n = 5 ; d=1/6 ; [x, F, iter, normSqF, dxSquared] = newtonsDiffusion( n, 1, 1e-12, 1e-12, 1e-12, 1000 ) ; plot( d:d:n * d, x )
-%     n=100 ; d=1/101 ; [x, F, iter, normSqF, dxSquared] = newtonsDiffusion( n, 8, 1e-12, 1e-12, 1e-12, 2000 ) ; plot( d:d:n * d, x )
+% n=100;d=1/101;V=20;ee=1e-12;[x, F, iter, normSqF, dxSquared] = newtonsDiffusion( n, V, ee, ee, ee, 1000 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
 %
 % solve: F(x) = G * x - b + 2 [sinh(x_i)]_i
 %        b(1) = -V, b(N) = V, b({else}) = 0.
@@ -33,9 +32,8 @@ function [x, F, iter, normSqF, dxSquared] = newtonsDiffusion( N, V, tolF, tolX, 
 nOnes = ones( N, 1 ) ;
 G = sparse( diag(2 * nOnes, 0) - diag(nOnes(1:N-1), -1) - diag(nOnes(1:N-1), 1) ) ;
 b = zeros( N, 1 ) ;
-lastX = b ;
+x = b ;
 
-% FIXME: See what may be ping-ponging at N=100,V=8,maxIter=2000
 b(1,1) = -V ;
 b(N,1) = V ;
 
@@ -45,50 +43,46 @@ deltaXsq = 1/(N+1)^2 ;
 %      = G * x - b + 2 * deltaXsq * sinh( x(1:N) )
 
 fp = 2 * deltaXsq ;
-J = G + sparse( diag(fp * nOnes, 0) ) ;
 
-% FIXME: use LU, but wait until I get base algorithm right.  
-fpinv = inv( J ) ;
-
-%disp( 'i & \\( \\alpha \\) & \\( \\Norm{ \\Bx } \\) & \\( \\Norm{ F(\\Bx)} \\) & \\( \\Norm{\\Delta x} \\) & \\( \\Norm{\\Delta x}/\\Norm{x} \\) \\\\ \\hline' ) ;
 disp( 'i & alpha & max |x_i| & |F| & |dx| & |dx|/|x|' ) ;
 iter = 0 ;
-x = lastX ;
 F = G * x - b + fp * sinh( x(1:N) ) ;
 dxSquared = 0 ;
 relDiffSqX = 0 ;
 normSqF = F.' * F ;
 
 while ( iter < maxIter )
-   delta = -fpinv * F ;
-   Gdelta = G * delta ;
-   Hprime = zeros(N, 1) ;
-
-   % can this statement be vectorized like sinh( x(1:N) ) ?
+   JF = zeros( N ) ;
    for i = 1:N
-      Hprime(i) = fp * x(i) * cosh( x(i) ) ;
+      JF(i,i) = fp * cosh( x(i) ) ;
    end
+   J = G + JF ;
 
-   Fprime = Gdelta + Hprime ;
-
-%disp( Gdelta.' * Fprime) ;
-%disp( Gdelta ) ;
-%disp( Fprime ) ;
-   alpha = -(F.' * Fprime) / ( Gdelta.' * Fprime) ;
+   delta = (-J)\F ;
+   
+%   Gdelta = G * delta ;
+%   Hprime = zeros(N, 1) ;
+%
+%   % can this statement be vectorized like sinh( x(1:N) ) ?
+%   for i = 1:N
+%      Hprime(i) = fp * x(i) * cosh( x(i) ) ;
+%   end
+%
+%   Fprime = Gdelta + Hprime ;
+%
+%   alpha = -(F.' * Fprime) / ( Gdelta.' * Fprime) ;
+alpha=1;
 
 %   if ( ( 0 == mod(iter, 50) ) || (iter < 10) )
       disp( sprintf( '%d & %f & %f & %2.1e & %2.1e & %2.1e \\\\ \\hline', iter, alpha, max(abs(x)), norm(F), sqrt(dxSquared), sqrt(relDiffSqX) ) ) ;
 %   end
 
-   x = lastX + delta ;
-%   x = lastX + alpha * delta ;
+   lastX = x ;
+   x = lastX + alpha * delta ;
 
-%disp( sprintf( '%d: x, H = ', iter ) ) ;
-% x(0) ends up with 800, for which isinf( sinh(800) ) = 1.
-%disp( x ) ;
+% If x(i) gets large (~800), this will trigger: isinf( sinh(800) ) = 1.
    H = fp * sinh( x(1:N) ) ;
-%disp( H ) ;
-%disp( {'H', H} ) ;
+
    F = G * x - b + H ;
    normSqF = F.' * F ;
    iter = iter + 1 ;
