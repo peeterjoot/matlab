5bf2a534c01f23e206ed92a44f73307c07d1e245
Peeter Joot <peeter.joot@gmail.com>
2014-11-08 14:25:04 -0500

    modified:   newtonsDiffusion.m

--- ece1254/ps2b/newtonsDiffusion.m
+++ ece1254/ps2b/newtonsDiffusion.m
@@ -1,16 +1,18 @@
-function [x, F, normF, normDx, totalIters, deltaX] = newtonsDiffusion( N, V, tolF, tolX, tolRel, maxIter, numStepIntervals )
+function [x, F, normF, normDx, totalIters, deltaX, beta, G] = newtonsDiffusion( N, V, tolF, tolX, tolRel, maxIter, numStepIntervals, noDamping )
 % sample calls:
 % 
-% V=1;ee=1e-6;[x, F, nF, dx, i, d] = newtonsDiffusion( 100, V, ee, ee, ee, 50, 1 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
-% (4 iters)
+% V=1;ee=1e-6;[x, F, nF, dx, i, d, nj] = newtonsDiffusion( 100, V, ee, ee, ee, 50, 1, 0 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
+% (. iters)
 %
-% V=20;ee=1e-6;[x, F, nF, dx, i, d] = newtonsDiffusion( 100, V, ee, ee, ee, 50, 1 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
-% (16 iters, 28 iters with 5 steps)
+% V=20;ee=1e-6;[x, F, nF, dx, i, d, nj] = newtonsDiffusion( 100, V, ee, ee, ee, 50, 1, 0 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
+% (. iters)
 %
-% V=100;ee=1e-6;[x, F, nF, dx, i, d] = newtonsDiffusion( 100, V, ee, ee, ee, 500, 1 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
-% (91 iters)
-% V=100;ee=1e-6;[x, F, nF, dx, i, d] = newtonsDiffusion( 100, V, ee, ee, ee, 500, 5 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
-% (37 iters)
+% V=100;ee=1e-6;[x, F, nF, dx, i, d, nj] = newtonsDiffusion( 100, V, ee, ee, ee, 500, 1, 0 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
+% (. iters)
+% V=100;ee=1e-6;[x, F, nF, dx, i, d, nj] = newtonsDiffusion( 100, V, ee, ee, ee, 500, 5, 0 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
+% (. iters)
+% V=100;ee=1e-6;[x, F, nF, dx, i, d, nj] = newtonsDiffusion( 100, V, ee, ee, ee, 500, 5, 1 ) ; y = [ -V;x;V ] ; plot( 0:d:1, y )
+% (. iters)
 %
 % solve: F(x) = G * x - b + 2 [sinh(x_i)]_i
 %        b(1) = -V, b(N) = V, b({else}) = 0.
@@ -20,13 +22,15 @@ function [x, F, normF, normDx, totalIters, deltaX] = newtonsDiffusion( N, V, tol
 %
 %    d^2 psi/du^2 = 2 sinh( psi(u) ), psi(0) = -V, psi(1) = V, u in [0,1]
 %
-% N: number of sampling points (not including end points).  solution vector x has components: x_i = psi( u_i )
+% N [in]: number of sampling points (not including end points).  solution vector x has components: x_i = psi( u_i )
 %
 % +-V: values at x=1,0
 %
-% numStepIntervals: 1 for no stepping.  Something higher to step the values of V from V/numStepIntervals to V.
+% numStepIntervals [in]: 1 for no stepping.  Something higher to step the values of V from V/numStepIntervals to V.
 %
-% maxIter: stop after this many iterations if not converged.
+% noDamping [in]: boolean: specify 1 to force alpha=1. 
+%
+% maxIter [in]: stop after this many iterations if not converged.
 %
 % iteration stops when all of:
 %    ||F|| < tolF
@@ -39,6 +43,8 @@ function [x, F, normF, normDx, totalIters, deltaX] = newtonsDiffusion( N, V, tol
 %    iter: last number of iterations
 %    normF: ||F(x)|| at the end of the iteration.
 %    normDx: ||x|| at the end of the iteration.
+%    beta: damping factor
+%    G: linear portion of the nodal matrix.
 
 nOnes = ones( N, 1 ) ;
 G = sparse( diag(2 * nOnes, 0) - diag(nOnes(1:N-1), -1) - diag(nOnes(1:N-1), 1) ) ;
@@ -59,7 +65,7 @@ disp( 'i & lambda & alpha & max |x_i| & |F| & |dx| & |dx|/|x|' ) ;
 
 lambdas = 1/numStepIntervals:1/numStepIntervals:1 ;
 
-beta = norm( full(inv(G)), 2 ) ;
+beta = sqrt(norm( full(inv(G)), 2 )) ;
 rho = hcoeff * V ;
 %maxForNewtons = 1/(beta^2 * rho) ;
 maxForNewtons = 1/(beta * rho) ;
@@ -85,7 +91,7 @@ for lambda = lambdas
 
       lastX = x ;
 
-      if ( normF <= maxForNewtons )
+      if ( (normF <= maxForNewtons) || noDamping )
          alpha = 1 ;
       else
          alpha = maxForNewtons / normF ;
@@ -96,9 +102,9 @@ for lambda = lambdas
       F = G * x - lambda * b + H ;
       normF = norm( F ) ;
 
-      if ( 0 == mod(iter, 50) || iter < 10 )
+%      if ( 0 == mod(iter, 50) || iter < 10 )
          disp( sprintf( '%d & %f & %f & %f & %2.1e & %2.1e & %2.1e \\\\ \\hline', iter, lambda, alpha, max(abs(x)), normF, normDx, relDiffX ) ) ;
-      end
+%      end
 
       iter = iter + 1 ;
       totalIters = totalIters + 1 ;
