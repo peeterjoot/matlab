3f83791735fea4ee6172ffd56755385b9652b322
Peeter Joot <peeter.joot@gmail.com>
2014-11-19 21:38:32 -0500

    modified:   nat.m

--- ece1254/ps3a/nat.m
+++ ece1254/ps3a/nat.m
@@ -1,15 +1,23 @@
 clear all ;
 [G, C, B, x] = NodalAnalysis( '../ps1/testdata/test2.netlist' ) ;
-% this was a manual test case, was verified manually.
+% a manual test case
 
 [G, C, B, x] = NodalAnalysis( '../ps1/testdata/ps1.circuit.netlist' ) ;
-% this was the circuit of ps1.
+% circuit of ps1.
 
+%-------------------
 % ps3a: example 1:
 [G, C, B, x] = NodalAnalysis( 'testdata/ps3a_sample1.netlist' ) ;
 
 % ps3a: example 2:
 [G, C, B, x] = NodalAnalysis( 'testdata/ps3a_sample2.netlist' ) ;
+
+% ps3a: example 3:
+[G, C, B, x] = NodalAnalysis( 'testdata/ps3a_sample3.netlist' ) ;
+
+% ps3a: example 3: with inductor sign reversed:
+[G, C, B, x] = NodalAnalysis( 'testdata/ps3a_sample3b.netlist' ) ;
+%-------------------
 
 % constant voltage source test case:
 [G, C, B, x] = NodalAnalysis( '../ps1/testdata/test3.netlist' ) ;
