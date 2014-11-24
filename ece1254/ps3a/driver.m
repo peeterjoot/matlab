337caf7003ec9933ebac88177611bb4070367f66
Peeter Joot <peeter.joot@gmail.com>
2014-11-24 16:15:53 -0500

    modified:   ps3a/driver.m modified:   ps3a/generateNetlist.m

--- ece1254/ps3a/driver.m
+++ ece1254/ps3a/driver.m
@@ -1,19 +1,25 @@
-function [G, C, B, xnames, s] = driver()
+function [G, C, b, xnames, s] = driver()
 
 filename = 'a.netlist' ;
 
-r = generateNetlist( filename ) ;
+%[r, endpoints] = generateNetlist( filename, [0.05 ; 0.05] ) ;
+[r, endpoints] = generateNetlist( filename ) ;
 
 [G, C, b, xnames] = NodalAnalysis( filename ) ;
 
+%deltaT = 0.5 ;
+%deltaT = 0.1 ;
 deltaT = 0.01 ;
+%deltaT = 0.005 ;
+%deltaT = 0.001 ;
 
-discreteTimes = [0:deltaT:0.25] ;
+%discreteTimes = [0:deltaT:0.25] ;
+discreteTimes = [0:deltaT:5] ;
 
 v = vclock(0) ;
 bv = b * v ;
 
-s = zeros( size(b, 1), size(discreteTimes, 2) ) ;
+s = [] ;
 
 CoverDeltaT = C/ deltaT ;
 A = G + CoverDeltaT ;
@@ -23,7 +29,7 @@ s(:,end+1) = x ;
 % prep for solving A\r
 [ L, U, P ] = lu( A ) ;
 
-for t = discreteTimes
+for t = discreteTimes(2:end)
    newV = vclock( t ) ;
    if ( newV ~= v )
       v = newV ;
@@ -36,3 +42,8 @@ for t = discreteTimes
    s(:,end+1) = x ;
 end
 
+%disp( endpoints(1) ) ;
+%disp( size(discreteTimes) ) ;
+%disp( size(s) ) ;
+%disp( s( 5, : ) ) ;
+plot( discreteTimes, s( endpoints(1), : ).' ) ;
--- ece1254/ps3a/generateNetlist.m
+++ ece1254/ps3a/generateNetlist.m
@@ -1,20 +1,60 @@
-function r = generateNetlist( filename )
+function [r, endpoints] = generateNetlist( filename, LengthsMM )
+% As test system, we consider the network in fig. 1.1 which distributes the clock signal to 8
+% blocks of an integrated circuit. The network is in the form of a binary tree with four levels.
+% Each segment is a transmission line with characteristics (length, per-unit-length parameters)
+% given in table 1.1.
+%
+% Level Length Resistance p.u.l Inductance p.u.l. Capacitance p.u.l.
+%       [mm]   R[Ω / cm]        L[nH / cm]        C[pF / cm]
+% 1     6      25               5.00              2.00
+% 2     4      35.7             7.14              1.40
+% 3     3      51.0             10.2              0.98
+% 4     2      51.0             10.2              0.98
+%
+% Divide each transmission line into segments of length ∆z = 0.05mm, and
+% model each segment with an RLC circuit as the one shown in the figure.
+%
+% Model the clock source with a periodic voltage source with the following characteristics: 
+% amplitude 1 V, rise/fall time 100 ps, period 2 ns, duty cycle 50%, initial delay 100 ps. The clock
+% source voltage is depicted in fig. 1.2.
+%
+% parameters:
+% [in] filename: netlist file to be generated is saved to this path.
+% [in] LengthsMM (optional):
+%     For test purposes, override the builtin LengthsMM with a different set of lengths.
+%     Example:
+%        LengthsMM          = [ 0.05 ; 0.05 ; 0.05 ; 0.05 ] ;           
+%        LengthsMM          = [ 0.05 ; 0.05 ] ;
+%        LengthsMM          = [ 0.15 ; 0.15 ; 0.15 ; 0.15 ] ;           
+%
+% LengthsMM must have at least 2 rows, and no more than 4.  The default value for this parameter is:
+%
+%   LengthsMM          = [ 6 ; 4 ; 3 ; 2 ] ;
+%
+% (that of the problem specification)
+% 
+% Output:
+%    r: table of in the RLC values per segment (to use in the problem set report).
+%    endpoints: nodenumbers for the loads on the circuit.  We are interested in the voltage at one of these points.
+%
+% Notes:
+% - clock signal is implemented in vclock().
 
-if ( 0 )
-   % for testing.  use less node numbers to verify the netlist node placement manually.
-   LengthsMM          = [ 0.05 ; 0.05 ; 0.05 ; 0.05 ] ;
-   %LengthsMM          = [ 0.15 ; 0.15 ; 0.15 ; 0.15 ] ;
-
-   maxLevel = 2 ;
-else
+if ( nargin < 2 )
    LengthsMM          = [ 6 ; 4 ; 3 ; 2 ] ;
-
-   maxLevel = 4 ;
 end
+maxLevel = size( LengthsMM, 1 ) ;
 
 ResistancePerCM    = [ 25 ; 35.7 ; 51.0 ; 51.0 ] ;
-InductancePerCM    = [ 5 ; 7.14 ; 10.2 ; 10.2 ] * 1e-9 ; % nH
+InductancePerCM    = [ 5 ; 7.14 ; 10.2 ; 10.2 ] * 1e-9 ; % nH (nanoHenry, one billionth (10^-9) of a Henry)
 CapacitancePerCM   = [ 2 ; 1.4 ; 0.98 ; 0.98 ] * 1e-12 ; % 1 pF (picofarad, one trillionth (10−12) of a farad)
+
+if ( maxLevel < 4 )
+   ResistancePerCM    = ResistancePerCM( 1:maxLevel ) ;
+   InductancePerCM    = InductancePerCM( 1:maxLevel ) ;
+   CapacitancePerCM   = CapacitancePerCM( 1:maxLevel ) ;
+end
+
 DeltaZMM           = 0.05 ;
 
 ResistancePerMM  = ResistancePerCM / 10 ;
