90042f43a30ca3dd9fd6320c67deab36e7937419
Peeter Joot <peeter.joot@gmail.com>
2014-12-09 23:39:17 -0500

    modified:   FourierMatrix.m modified:   Gprime.m modified:   HBSolve.m modified:   HarmonicBalance.m modified:   gnl.m

--- ece1254/HarmonicBalanceBigF/Gprime.m
+++ ece1254/HarmonicBalanceBigF/Gprime.m
@@ -1,69 +1,66 @@
 function [ Gprime ] = Gprime( bdiodes, x, Nh , Nm )
-%Gprime This function produces the nonlinear contribution to the Jacobean
-%required to solve a nonlinear circuit using Newton's Method
-%   bdiodes is a cell matrix containing the infromation describing the
-%   location and parameters of all diodes in the circuit
-%   x is the vector of all unknown quantities
-%   Nh is the number of harmonics used in the analysis
-%   Nm is the number of physical unknowns of the MNA
+   %Gprime This function produces the nonlinear contribution to the Jacobean
+   %required to solve a nonlinear circuit using Newton's Method
+   %   bdiodes is a cell matrix containing the infromation describing the
+   %   location and parameters of all diodes in the circuit
+   %   x is the vector of all unknown quantities
+   %   Nh is the number of harmonics used in the analysis
+   %   Nm is the number of physical unknowns of the MNA
 
-N = length(bdiodes) ;
-F = FourierMatrix(Nh,Nm) ;
+   N = length(bdiodes) ;
+   F = FourierMatrix(Nh,Nm) ;
 
-dDiode = @(v,io,Vt) (io/Vt)*(exp(v/Vt)) ;
+   dDiode = @(v,io,Vt) (io/Vt)*(exp(v/Vt)) ;
 
-%Generate the Jacobian corresponding to a single circuit for each time step
-%then combine them as a block column matrix. This form allows the fourier
-%matrix to be used in obtaining the DFT
+   %Generate the Jacobian corresponding to a single circuit for each time step
+   %then combine them as a block column matrix. This form allows the fourier
+   %matrix to be used in obtaining the DFT
 
-gprime = zeros(Nm*(2*Nh+1)) ;
-%for each time step
-for j = 0:2*Nh ;
-    gprimecell = zeros(Nm) ;
-%for each diode
-    for i = 1:N ;
-        d = bdiodes{i} ;
+   gprime = zeros(Nm*(2*Nh+1)) ;
+   %for each time step
+   for j = 0:2*Nh ;
+       gprimecell = zeros(Nm) ;
+   %for each diode
+       for i = 1:N ;
+           d = bdiodes{i} ;
 
-        n1 = d.vp ;
-        n2 = d.vn ;
+           n1 = d.vp ;
+           n2 = d.vn ;
 
-        io = abs(d.io) ;
-        Vt = abs(d.vt) ;
-        v = 0 ;
+           io = abs(d.io) ;
+           Vt = abs(d.vt) ;
+           v = 0 ;
 
-        if (n1)
-            v = v + x( n1 + j*Nm ) ;
-        end
+           if (n1)
+               v = v + x( n1 + j*Nm ) ;
+           end
 
-        if (n2)
-            v = v - x( n2 + j*Nm ) ;
-        end
+           if (n2)
+               v = v - x( n2 + j*Nm ) ;
+           end
 
-        dg = dDiode(v,io,Vt) ;
+           dg = dDiode(v,io,Vt) ;
 
-        % insert the stamp:
-        if ( n1 )
-            gprimecell( n1, n1 ) = gprimecell( n1, n1 ) + dg ;
-            if ( n2 )
-                gprimecell( n1, n2 ) = gprimecell( n1, n2 ) - dg ;
-                gprimecell( n2, n1 ) = gprimecell( n2, n1 ) - dg ;
-            end
-        end
-        if ( n2 )
-            gprimecell( n2, n2 ) = gprimecell( n2, n2 ) + dg ;
-        end
-    end
+           % insert the stamp:
+           if ( n1 )
+               gprimecell( n1, n1 ) = gprimecell( n1, n1 ) + dg ;
+               if ( n2 )
+                   gprimecell( n1, n2 ) = gprimecell( n1, n2 ) - dg ;
+                   gprimecell( n2, n1 ) = gprimecell( n2, n1 ) - dg ;
+               end
+           end
+           if ( n2 )
+               gprimecell( n2, n2 ) = gprimecell( n2, n2 ) + dg ;
+           end
+       end
 
-    corner = 1 + j*Nm ;
-    
+       corner = 1 + j*Nm ;
+       
 
-    gprime(corner:corner+Nm-1,corner:corner+Nm-1) = gprimecell ;
-    
+       gprime(corner:corner+Nm-1,corner:corner+Nm-1) = gprimecell ;
+       
+   end
+
+   Gprime = F\(gprime*F) ;
+
 end
-
-Gprime = F\(gprime*F) ;
-
-end
-
-
-
--- ece1254/HarmonicBalanceBigF/HarmonicBalance.m
+++ ece1254/HarmonicBalanceBigF/HarmonicBalance.m
@@ -1,100 +1,100 @@
 function [Y, Vnames, I, bdiode] = HarmonicBalance(G, C, B, bdiode, angularVelocities, xnames, N, omega)
-% HarmonicBalance generates the Harmonic balance modified nodal analysis (MNA) equations from the time domain MNA
-% representation.
-%
-%    Y V = B U = I + ~I,                                        (1)
-%
-% where ~I represents non-linear contributions not returned directly as a matrix.
-%
-% The Harmonic balance results returned are associated with the time domain equations
-%
-%    G x(t) + C \dot{x}(t)= B angularVelocities(t),             (2)
-%
-% as returned from HarmonicBalance(),
-% where the column vector angularVelocities(t) contains all sources, and x(t) is a vector of all the sources.
-%
-% Here V = [
-%    V_{-N}^(1)
-%    V_{-N}^(2)
-%    .
-%    .
-%    .
-%    V_{1-N}^(1)
-%    V_{1-N}^(2)
-%    .
-%    .
-%    .
-%    V_{0}^(1)
-%    V_{0}^(2)
-%    .
-%    .
-%    .
-%    V_{N-1}^(1)
-%    V_{N-1}^(2)
-%    .
-%    .
-%    .
-%    V_{N}^(1)
-%    V_{N}^(2)
-%    .
-%    .
-%    .
-%   ]
-%
-% is a vector of DFT Fourier coefficients, defined by the transform pair:
-%
-%     x_k = \sum_{n = -N}^N X_k e^{j omega n t_k}                    (3)
-%     X_k = (1/(2 N + 1)) \sum_{n = -N}^N x_k e^{-j omega n t_k}     (4)
-%     t_k = T k/(2 N + 1)                                            (5)
-%     omega T = 2 pi                                                 (6)
-%
-% Y has the block diagonal structure
-%
-%    Y = [ {G + j omega n}_n \delta_{nm} ]_{nm}                      (7)
-%
-%---------------------------------------------------------------------------------------
-%
-% INPUT PARAMETERS:
-%
-% This function consumes all the output parameters of:
-%
-%    [G, C, B, bdiode, angularVelocities, xnames] = NodalAnalyis()
-%
-% which should be passed here in the same order.  In addition to those, also pass:
-%
-% - N [integer]:
-%
-%     The number of frequencies to include in the bandwith limited Fourier representation (3)
-%
-% - omega [double]:
-%
-%     The base frequency for all the higher level harmonics.
-%
-%------------------------------------------------
-%
-% With R equal to the total number of MNA variables, the returned parameters
-%
-% - Y [array]
-%
-%    R(2N+1) x R(2N+1) matrix, where the 2N+1 RxR matrices down the diagonal are formed from sums of G's and (j omega n C)'s
-%
-% - I [array]
-%
-%    R x (2 N + 1) matrix of linear source Fourier coefficients.
-%
-% - Vnames [cell]
-%
-%   is an R x (2 N + 1) array of strings for each of the Fourier coefficient variables in the frequency domain equations.
-%
-%   The R entries are composed of:
-%   - Entries for each node voltage in the system.
-%   - Entries for each current variable flowing through a voltage source, a voltage
-%     controlled voltage source, an inductor, or a diode (this last also treated as a current source).
-%     When there are diodes, there will also be a non-linear portion of the diode model to handle separately.
-%
-% - fixme: handle non-linear diode stuff and return something for that.
-%
-%------------------------------------------------
+   % HarmonicBalance generates the Harmonic balance modified nodal analysis (MNA) equations from the time domain MNA
+   % representation.
+   %
+   %    Y V = B U = I + ~I,                                        (1)
+   %
+   % where ~I represents non-linear contributions not returned directly as a matrix.
+   %
+   % The Harmonic balance results returned are associated with the time domain equations
+   %
+   %    G x(t) + C \dot{x}(t)= B angularVelocities(t),             (2)
+   %
+   % as returned from HarmonicBalance(),
+   % where the column vector angularVelocities(t) contains all sources, and x(t) is a vector of all the sources.
+   %
+   % Here V = [
+   %    V_{-N}^(1)
+   %    V_{-N}^(2)
+   %    .
+   %    .
+   %    .
+   %    V_{1-N}^(1)
+   %    V_{1-N}^(2)
+   %    .
+   %    .
+   %    .
+   %    V_{0}^(1)
+   %    V_{0}^(2)
+   %    .
+   %    .
+   %    .
+   %    V_{N-1}^(1)
+   %    V_{N-1}^(2)
+   %    .
+   %    .
+   %    .
+   %    V_{N}^(1)
+   %    V_{N}^(2)
+   %    .
+   %    .
+   %    .
+   %   ]
+   %
+   % is a vector of DFT Fourier coefficients, defined by the transform pair:
+   %
+   %     x_k = \sum_{n = -N}^N X_k e^{j omega n t_k}                    (3)
+   %     X_k = (1/(2 N + 1)) \sum_{n = -N}^N x_k e^{-j omega n t_k}     (4)
+   %     t_k = T k/(2 N + 1)                                            (5)
+   %     omega T = 2 pi                                                 (6)
+   %
+   % Y has the block diagonal structure
+   %
+   %    Y = [ {G + j omega n}_n \delta_{nm} ]_{nm}                      (7)
+   %
+   %---------------------------------------------------------------------------------------
+   %
+   % INPUT PARAMETERS:
+   %
+   % This function consumes all the output parameters of:
+   %
+   %    [G, C, B, bdiode, angularVelocities, xnames] = NodalAnalyis()
+   %
+   % which should be passed here in the same order.  In addition to those, also pass:
+   %
+   % - N [integer]:
+   %
+   %     The number of frequencies to include in the bandwith limited Fourier representation (3)
+   %
+   % - omega [double]:
+   %
+   %     The base frequency for all the higher level harmonics.
+   %
+   %------------------------------------------------
+   %
+   % With R equal to the total number of MNA variables, the returned parameters
+   %
+   % - Y [array]
+   %
+   %    R(2N+1) x R(2N+1) matrix, where the 2N+1 RxR matrices down the diagonal are formed from sums of G's and (j omega n C)'s
+   %
+   % - I [array]
+   %
+   %    R x (2 N + 1) matrix of linear source Fourier coefficients.
+   %
+   % - Vnames [cell]
+   %
+   %   is an R x (2 N + 1) array of strings for each of the Fourier coefficient variables in the frequency domain equations.
+   %
+   %   The R entries are composed of:
+   %   - Entries for each node voltage in the system.
+   %   - Entries for each current variable flowing through a voltage source, a voltage
+   %     controlled voltage source, an inductor, or a diode (this last also treated as a current source).
+   %     When there are diodes, there will also be a non-linear portion of the diode model to handle separately.
+   %
+   % - fixme: handle non-linear diode stuff and return something for that.
+   %
+   %------------------------------------------------
 
    traceit( ['N, omega: ', N, omega] ) ;
 
