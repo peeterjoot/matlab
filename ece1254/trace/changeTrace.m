59482eb0e2a59b66f5213637d69d606a4c14c36c
Peeter Joot <peeter.joot@gmail.com>
2014-12-18 16:16:25 -0500

    update makefiles to use ../pd/make.*
    modified:   ../../METADATA
    renamed:    ../trace/README.txt -> ../pd/README.txt
    renamed:    ../ps1/changeTrace.m -> ../pd/changeTrace.m
    renamed:    ../ps1/disableTrace.m -> ../pd/disableTrace.m
    renamed:    ../ps1/enableDebug.m -> ../pd/enableDebug.m
    renamed:    ../ps1/enableTrace.m -> ../pd/enableTrace.m
    renamed:    ../ps1/isDebugEnabled.m -> ../pd/isDebugEnabled.m
    new file:   ../pd/make.rules
    renamed:    ../trace/make.frag -> ../pd/make.vars
    renamed:    ../ps1/traceit.m -> ../pd/traceit.m
    modified:   GNUmakefile
    modified:   ../ps1/GNUmakefile
    modified:   ../ps2a/GNUmakefile
    modified:   ../ps2b/GNUmakefile
    modified:   ../ps3a/GNUmakefile
    modified:   ../ps3b/GNUmakefile
    deleted:    ../trace/changeTrace.m
    deleted:    ../trace/disableTrace.m
    deleted:    ../trace/enableDebug.m
    deleted:    ../trace/enableTrace.m
    deleted:    ../trace/isDebugEnabled.m
    deleted:    ../trace/traceit.m

--- ece1254/trace/changeTrace.m
+++
@@ -1,7 +0,0 @@
-function changeTrace( v )
-% enable or disable trace
-%
-   global g_bTraceOn ;
-
-   g_bTraceOn = v ;
-end
--- ece1254/trace/enableTrace.m
+++
@@ -1,6 +0,0 @@
-function enableTrace( )
-% enable debug tracing.
-%    Sets a global trace variable to non-zero so that calls to traceit( ... ) don't print anything.
-
-   changeTrace( 1 ) ;
-end
