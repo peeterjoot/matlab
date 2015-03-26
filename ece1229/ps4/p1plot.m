f46b5decd54a6915f1fa3ddff5c0ea8344dfd288
Peeter Joot <peeter.joot@gmail.com>
2015-03-25 22:35:09 -0400

    modified:   GNUmakefile modified:   p1plot.m new file:   p2.m

--- ece1229/ps4/p1plot.m
+++ ece1229/ps4/p1plot.m
@@ -5,7 +5,7 @@ function f = p1plot( ad )
 
    theta = 0:0.01:pi ;
 
-   af = ((1 + exp( -1j * ( kd * cos( theta ) + ad ) )).^2/4) ;
+   af = ((1 + exp( 1j * ( kd * cos( theta ) + ad ) )).^2/4) ;
 
    afabs = abs( af ) ;
 %   afsq = af .* conj(af) ;
---
+++ ece1229/ps4/p2.m
@@ -0,0 +1,37 @@
+function p2( )
+   % p2: Problem 2.  Code to confirm the zeros numerically, and to plot the absolute array factor.
+
+   kd = 2 * pi * (1/2) ;
+
+   theta = 0:0.01:pi ;
+   thetaZeros = [0, pi/3, 2 * pi/3 ] ;
+
+   expValues = exp( 1j * ( kd * cos( theta ) ) ) ;
+   expValuesZ = exp( 1j * ( kd * cos( thetaZeros ) ) ) ;
+
+   af = abs((1 + expValues + expValues.^2 + expValues.^3)/4) ;
+   afz = (1 + expValuesZ + expValuesZ.^2 + expValuesZ.^3)/4 ;
+
+   cosValues = abs( cos( kd * cos( theta )/2 ) + cos( 3 * kd * cos( theta )/2 ) )/2 ;
+   cosValuesZ = (cos( kd * cos( thetaZeros )/2 ) + cos( 3 * kd * cos( thetaZeros )/2 ))/2 ;
+
+   f = figure ;
+
+   if ( 0 )
+      polar( theta, af ) ;
+   else
+      polar( theta, cosValues ) ;
+   end
+
+% manual unit test: confirm the zeros:
+   if ( 0 )
+      disp( afz )
+      disp( cosValuesZ )
+   end
+
+   [fileExtension, savePlot] = saveHelper() ;
+
+   saveName = sprintf( 'ps4p2PlotFig1.%s', fileExtension ) ;
+
+   savePlot( f, saveName ) ;
+end
