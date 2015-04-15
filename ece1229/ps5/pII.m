cf0a4823f13118de061f968f80e66590526eb1b8
Peeter Joot <peeter.joot@gmail.com>
2015-04-15 08:43:34 -0400

    modified:   pII.m

--- ece1229/ps5/pII.m
+++ ece1229/ps5/pII.m
@@ -1,37 +1,5 @@
 
-   r = calculateZinAndStuff( 2 ) ;
-
-   %------------------------------
-   % (a)
-   %
-   % m->cm
-   r.W
-
-   %------------------------------
-   % (b)
-   %
-   r.eEff
-
-   %------------------------------
-   % (c)
-   %
-   r.L0 
-
-   %------------------------------
-   % (d)
-   % cm
-   disp('L') ;
-   r.L
-
-   %------------------------------
-   % (e)
-
-   disp( 'k0 lambda0 G B Ys' ) ;
-   r.k0
-   r.lambda0
-   r.G
-   r.B
-   r.Ys
+   r = calculateZinAndStuff( 2 )
 
    YsRadius = abs(r.Ys)
    YsDegrees = angle(r.Ys) * 180/pi
@@ -39,27 +7,18 @@
    %------------------------------
    % (f)
    %
-   r.Zs
    ZsRadius = abs(r.Zs)
    ZsDegrees = angle(r.Zs) * 180 /pi
-   r.beta
 
-   r.Zin2
    Zin2Radius = abs(r.Zin2)
    Zin2Degrees = angle(r.Zin2) * 180 /pi
 
-   r.Yin2
    Yin2Radius = abs(r.Yin2)
    Yin2Degrees = angle(r.Yin2) * 180 /pi
-
-   %disp('hOverLambda0') ;
-   %r.hOverLambda0
 
    %------------------------------
    % (g)
    %
-   r.Ytotal
-   r.Zin
    ZinRadius = abs(r.Zin)
    ZinDegrees = angle(r.Zin) * 180 /pi
 
